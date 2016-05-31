7e9089df02f7651c6380a2a4b23d20b2cf5e4e57
jean-michel Delorme <jm.delorme@ymail.com>
2016-05-31 15:04:26 +0200

    Unit Test Transfert

--- src/test/java/fr/jmdel/mBank/TransactionTest.java
+++ src/test/java/fr/jmdel/mBank/TransactionTest.java
@@ -7,8 +7,35 @@ import org.junit.Test;
 public class TransactionTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void TranfertNotEnouthMoneyTest() {
+		//Given
+		Compte compte1 = new Compte(00001);
+		compte1.setSolde(500);
+		Compte compte2 = new Compte(00002);
+		
+		Transaction transaction = new Transaction();
+		
+		//When
+		boolean valid=transaction.transfert(compte1, compte2, 700);
+		//Then
+		assertEquals(valid,false);
+		
+	}
+	
+	@Test
+	public void TranfertOneNullAcountTest() {
+		//Given
+		Compte compte1 = new Compte(00001);
+		
+		Compte compte2 = null;
+		
+		Transaction transaction = new Transaction();
+		
+		//When
+		boolean valid=transaction.transfert(compte1, compte2, 0);
+		//Then
+		assertEquals(valid,false);
+		
 	}
 
 }
