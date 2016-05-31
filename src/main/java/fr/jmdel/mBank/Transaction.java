dc6bee4a2c25ab50fcd52aafdd660800e120926f
jean-michel Delorme <jm.delorme@ymail.com>
2016-05-31 14:31:26 +0200

    add transaction, transactionTest class and c

---
+++ src/main/java/fr/jmdel/mBank/Transaction.java
@@ -0,0 +1,24 @@
+package fr.jmdel.mBank;
+
+public class Transaction {
+
+	public boolean transfert(Compte compte1, Compte compte2, int amount){
+	boolean	transactionOK=false;
+	if((compte1!=null)&&(compte1!=null)){
+		int initialSold1 = compte1.getSolde();
+		if(amount<=initialSold1){
+		
+		compte1.setSolde(initialSold1-amount);
+		
+		int initialSold2 = compte2.getSolde();
+		compte2.setSolde(initialSold2+amount);
+		
+		transactionOK=true;
+		}
+	}
+		
+		
+	return 	transactionOK;
+	}
+
+}
